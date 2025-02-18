81ee76e4b92035780d8a1f74f906281679354d58
Michael4Camps <C24354791@myTUDublin.ie>
2025-02-18 17:56:04 +0000

    MousePosition
    Use for the gun.

--- shootergame/Scripts/Player_Script.gd
+++ shootergame/Scripts/Player_Script.gd
@@ -9,6 +9,7 @@ extends CharacterBody2D
 #@onready var animated_sprite_2d: AnimatedSprite2D =$"."
 
 @export var speed = 400
+#var MousePos:float = get_global_mouse_position().x
 
 func get_input():
 	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
@@ -17,3 +18,9 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+	
+	
+	
+	if Input.is_action_just_pressed("Shoot"):
+		get_global_mouse_position()
+		print()
