32d2adf24d9ac08ef350a42deea0fc897229f330
Michael4Camps <C24354791@myTUDublin.ie>
2025-02-20 12:16:23 +0000

    Simple reload system

--- shootergame/Scripts/Player_Script.gd
+++ shootergame/Scripts/Player_Script.gd
@@ -9,9 +9,10 @@ extends CharacterBody2D
 #@onready var animated_sprite_2d: AnimatedSprite2D =$"."
 @onready var shooting_point: Marker2D = $Polygon2D/ShootingPoint
 @export var speed = 500
-var bullet_speed = 2000
+var bullet_speed = 6000
 var bullet = preload("res://Scenes/Bullet.tscn")
-var ammo:int = 15
+var reserve_ammo:int = 10
+var mag_ammo:int = 2
 
 
 var Can_Fire:bool = true
@@ -22,9 +23,14 @@ func get_input():
 	velocity = input_direction * speed
 	look_at(get_global_mouse_position())
 	
-	if Input.is_action_just_pressed("Shoot") and Can_Fire and ammo > 0:
+	if Input.is_action_just_pressed("Shoot") and Can_Fire and mag_ammo > 0:
 		fire()
-		ammo = ammo - 1
+		mag_ammo = mag_ammo - 1
+		
+	if Input.is_action_just_pressed("Reload") and reserve_ammo > 0:
+		mag_ammo = mag_ammo + 2
+		reserve_ammo = reserve_ammo - 2
+		
 	
 
 
@@ -39,4 +45,7 @@ func fire():
 	bullet_instance.rotation_degrees = rotation_degrees
 	#bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
 	get_tree().get_root().call_deferred("add_child", bullet_instance)
+	
+#func reload():
+	
 	
