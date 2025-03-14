1f1841715db5f8381bcfbe875a8ed2c941dfc1e2
Michael4Camps <C24354791@myTUDublin.ie>
2025-03-14 11:15:44 +0000

    Update

--- shootergame/Scripts/Player_Script.gd
+++ shootergame/Scripts/Player_Script.gd
@@ -21,6 +21,8 @@ var ammo_diff:int
 
 var ammo_pack:bool = true
 
+var fire_damage:float = 0.0
+
 func get_input():
 	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
 	velocity = input_direction * speed
@@ -80,12 +82,22 @@ func _on_health_pack_body_entered(body: Node2D) -> void:
 	pass # Replace with function body.
 	
 
-
-
-
 func _on_damage_aera_body_entered(body: Node2D) -> void:
 	print("Entered")
-	player_Health = player_Health - 25
+	$"../FireDamage".start()
+	player_Health = player_Health - (5 * fire_damage)
 	#$"../DamageAera".queue_free()
+	pass # Replace with function body.
 	
+#func _on_damage_aera_body_exited(body: Node2D) -> void:
+	#$"../FireDamage".stop()
+	
+	
+	pass # Replace with function body.
+
+
+
+
+func _on_fire_damage_timeout() -> void:
+	fire_damage += 0.2
 	pass # Replace with function body.
