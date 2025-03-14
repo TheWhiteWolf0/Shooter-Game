2d66a40f956956881c53e3d661dcb27bad512d8a
Michael4Camps <C24354791@myTUDublin.ie>
2025-03-14 12:45:10 +0000

    update
    works

--- shootergame/Scripts/Player_Script.gd
+++ shootergame/Scripts/Player_Script.gd
@@ -87,13 +87,9 @@ func _on_health_pack_body_entered(body: Node2D) -> void:
 func _on_damage_aera_body_entered(body: Node2D) -> void:
 	print("Entered")
 	fireDamage = true
-	#$"../FireDamage".start()
-	#fire_damage = fire_damage + 0.2
-	print("Fire Damage is True")
-	#player_Health = player_Health - (5 * fire_damage)
 	if fireDamage == true:
 		$"../FireDamage".start()
-		fire_damage = fire_damage + 0.2
+		fire_damage = fire_damage + 1
 		player_Health = player_Health - (5 * fire_damage)
 		
 	
@@ -104,11 +100,3 @@ func _on_damage_aera_body_entered(body: Node2D) -> void:
 func _on_fire_damage_timeout() -> void:
 	fire_damage += 0.2
 	pass # Replace with function body.
-	#get_process_delta_time()
-
-#func _process(delta: float) -> void:
-#func _Damage():
-	#while fireDamage == true:
-		#player_Health = player_Health - (1 * fire_damage)
-	
-	
