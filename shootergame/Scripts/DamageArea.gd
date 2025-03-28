10af1136c82ceca6e45fba46fa7cd8d6027204d4
Michael4Camps <C24354791@myTUDublin.ie>
2025-03-28 10:30:10 +0000

    Damage OverTime Works
    Timer are being used. I now know how to use them very good very nice

--- shootergame/Scripts/DamageArea.gd
+++ shootergame/Scripts/DamageArea.gd
@@ -15,7 +15,20 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	print("Entered")
 	GlobalVariables.damage_Overtime = true
+	#if GlobalVariables.damage_Overtime == true:
+		#_on_timer_timeout()
+		#print("KaChow")
+		#GlobalVariables.damage_OT_Fire = GlobalVariables.damage_OT_Fire + 1
+		#GlobalVariables.player_Health = GlobalVariables.player_Health - (5 * GlobalVariables.damage_OT_Fire)
+	pass # Replace with function body.
+
+
+
+
+func _on_timer_timeout() -> void:
+	print("Hello world")
 	if GlobalVariables.damage_Overtime == true:
 		GlobalVariables.damage_OT_Fire = GlobalVariables.damage_OT_Fire + 1
 		GlobalVariables.player_Health = GlobalVariables.player_Health - (5 * GlobalVariables.damage_OT_Fire)
+		
 	pass # Replace with function body.
