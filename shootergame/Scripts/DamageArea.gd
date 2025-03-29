a8a7ece47556fe1fb551b9825d1d0776659ff097
Michael4Camps <C24354791@myTUDublin.ie>
2025-03-29 22:56:23 +0000

    turned off auto start for the timer

--- shootergame/Scripts/DamageArea.gd
+++ shootergame/Scripts/DamageArea.gd
@@ -26,7 +26,7 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func _on_timer_timeout() -> void:
-	print("Hello world")
+	#print("Hello world")
 	if GlobalVariables.damage_Overtime == true:
 		GlobalVariables.damage_OT_Fire = GlobalVariables.damage_OT_Fire + 1
 		GlobalVariables.player_Health = GlobalVariables.player_Health - (5 * GlobalVariables.damage_OT_Fire)
