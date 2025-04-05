0b993f9b9557899232556b80bd641be2ba05f630
Michael4Camps <C24354791@myTUDublin.ie>
2025-04-05 20:41:41 +0100

    update

--- shootergame/Scripts/Spawner.gd
+++ shootergame/Scripts/Spawner.gd
@@ -1,28 +1,16 @@
-
-
-
-
-
-
-
-
-
 extends Node2D
 
-var mob_chase = preload("res://Scenes/enemy(chase).tscn")
+
+
+
+
+
+
+@onready var enemy_chase = preload("res://Scenes/enemy(chase).tscn")
+
 @onready var spawn_point: Marker2D = $SpawnPoint
 
-
 func _on_timer_timeout() -> void:
-	print("Hello World")
-	var ene = mob_chase.instantiate()
-	ene.position = position
-	get_tree().get_root().call_deferred("add_child", ene)
-	#get_parent().get_node().add_child(ene)
-	
-	pass # Replace with function body.
-	
-#func _MobSpawner():
-	#var mob_instance = mob_chase.instantiate()
-	#mob_chase.global_position = spawn_point.global_position
-	#get_tree().get_root().call_deferred("add_child", mob_instance)
+	var ene_chase = enemy_chase.instantiate()
+	ene_chase.position = spawn_point.position
+	get_parent().get_node("AllMobs").add_child(enemy_chase)
