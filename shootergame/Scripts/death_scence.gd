57af407db53d9341033059e04d3d912bf5962c73
Michael4Camps <C24354791@myTUDublin.ie>
2025-04-20 23:42:25 +0100

    Added upgrade button to DeathScence

--- shootergame/Scripts/death_scence.gd
+++ shootergame/Scripts/death_scence.gd
@@ -25,3 +25,7 @@ func _on_button_2_pressed() -> void:
 	GlobalVariables.reserve_ammo = GlobalVariables.reserve_ammo + 20
 	get_tree().change_scene_to_file("res://Scenes/lvl2.tscn")
 	pass # Replace with function body.
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/control.tscn")
+	pass # Replace with function body.
